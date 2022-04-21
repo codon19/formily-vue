303b669113e07e96f747332fd83d53edd93bc49e
songguanghui <1554095532@qq.com>
2022-04-21 16:54:51 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,117 @@
 <template>
-  <div id="app">
-    <router-view />
+  <div id="#app">
+    <Form :form="form">
+      <SchemaField :schema="schema" :scope="{ useProvinceDataSource, useCityDataSource }" />
+      <Submit @submit="onSubmit">提交</Submit>
+      <Reset @click="onReset">重置</Reset>
+    </Form>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
+<script>
+import { createForm, onFieldValueChange, onFieldReact } from '@formily/core'
+import { createSchemaField } from '@formily/vue'
+import { Form, FormItem, Select, Input, Submit, Reset } from '@formily/element'
+import areaData from './utils/area'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+const schema = {
+  type: 'object',
+  properties: {
+    province: {
+      type: 'number',
+      title: '省',
+      'x-decorator': 'FormItem',
+      'x-component': 'Select',
+      'x-component-props': {
+        style: 'width: 240px;',
+        clearable: true
+      },
+      'x-reactions': ['{{useProvinceDataSource()}}']
+    },
+    city: {
+      type: 'number',
+      title: '市',
+      'x-decorator': 'FormItem',
+      'x-component': 'Select',
+      'x-component-props': {
+        style: 'width: 240px;',
+        clearable: true
+      },
+      'x-reactions': ['{{useCityDataSource()}}']
+    },
+    addressDetails: {
+      type: 'number',
+      title: '详细地址',
+      'x-decorator': 'FormItem',
+      'x-component': 'Input',
+      'x-component-props': {
+        style: 'width: 240px;',
+        placeholder: '请输入'
+      },
+      'x-display': 'none'
     }
   }
 }
-</style>
+
+// 设置省枚举值
+const useProvinceDataSource = () => {
+  return field => {
+    field.dataSource = areaData.map(({ label, value }) => ({ label, value }))
+  }
+}
+
+// 设置市枚举值
+const useCityDataSource = () => {
+  return field => {
+    const componentType = field.query('province').get('value')
+    if (!componentType) return []
+    const findItem = areaData.find(item => item.value === componentType)
+    field.dataSource = findItem ? findItem.children : []
+  }
+}
+
+const form = createForm({
+  values: {
+    province: 2,
+    city: 21
+  },
+  effects() {
+    onFieldValueChange('province', () => {
+      form.setValuesIn('city', '')
+    })
+    onFieldReact('addressDetails', field => {
+      const city = field.query('city').value()
+      // 当选择的城市是 南京 时，显示详细地址输入框
+      field.display = city === 21 ? 'visible' : 'none'
+    })
+  }
+})
+const { SchemaField } = createSchemaField({
+  components: {
+    FormItem,
+    Select,
+    Input
+  }
+})
+
+export default {
+  name: 'SearchForm',
+  components: { Form, SchemaField, Submit, Reset },
+  data() {
+    return {
+      form,
+      schema
+    }
+  },
+  methods: {
+    useProvinceDataSource,
+    useCityDataSource,
+    onSubmit(value) {
+      console.log(value)
+    },
+    onReset() {
+      console.log('onReset')
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
 import store from './store'
 import Element from 'element-ui'
 Vue.use(Element)
@@ -8,7 +7,6 @@ Vue.use(Element)
 Vue.config.productionTip = false
 
 new Vue({
-  router,
   store,
   render: h => h(App)
 }).$mount('#app')
